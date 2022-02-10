71da2ef728f4d86a9ca580489d87197a6182db69
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-10 10:34:02 +0530

    [Arjun] Add ; Added Executed

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,34 @@
+﻿using BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the binary search tree programs");
+            Console.WriteLine("\n1.Insertion.");
+            Console.WriteLine("Enter the option!!!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            BST<int> binarySearchTree = new BST<int>(56);
+            switch (num)
+            {
+                case 1:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
